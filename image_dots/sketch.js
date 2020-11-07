918b0ee0a98019649b89d70beffcdf264bca77cc
Matteo Filippini <34274990+Mattemn97@users.noreply.github.com>
2020-11-08 00:41:35 +0100

    Update sketch.js

--- image_dots/sketch.js
+++ image_dots/sketch.js
@@ -1,29 +1,32 @@
+var font;
 var vehicles = [];
-var images = [];
-var imgData;
 
-let img;
 function preload() {
-  img = loadImage('images/Profilo.jpg');
-  imgData = ctx.getImageData(x, y, width, height).data;
+    font = loadFont('DS-DIGI.TTF');
 }
 
 function setup() {
     createCanvas(1000, 300);
     background('#265473');
-    image(img, 10, 10, 50, 50);
+    setInterval(secondDraw, 1000);
+    prima = true;
 }
 
 function draw() {
-    var points 
-    for (var i = 0; i < points.length; i++) {
-        var pt = points[i];
-        var vehicle = new Vehicle(pt.x, pt.y);
-        vehicles.push(vehicle);
+    if (prima) {
+        var time = gettime();
+        var points = font.textToPoints(time, 100, 200, 192, {
+            sampleFactor: 0.25
+        });
+        for (var i = 0; i < points.length; i++) {
+            var pt = points[i];
+            var vehicle = new Vehicle(pt.x, pt.y);
+            vehicles.push(vehicle);
+        }
+        prima = false;
+        console.log("Prima");
+        console.log("Lunghezza vehicles:" + vehicles.length);
     }
-    prima = false;
-    console.log("Prima");
-    console.log("Lunghezza vehicles:" + vehicles.length);
     background('#265473');
     for (var i = 0; i < vehicles.length; i++) {
         var v = vehicles[i];
@@ -31,4 +34,33 @@ function draw() {
         v.update();
         v.show();
     }
+}
+
+function secondDraw() {
+    var time = gettime();
+    var points = font.textToPoints(time, 100, 200, 192, {
+        sampleFactor: 0.25
+    });
+
+    if (points.length <= vehicles.length) {
+        for (var k = 0; k < points.length; k++) {
+            vehicles[k].visible = true;
+            var new_pt1 = createVector(points[k].x, points[k].y);
+            vehicles[k].changeTarget(new_pt1.x, new_pt1.y);
+        }
+        for (var j = points.length; j < vehicles.length; j++) {
+            vehicles[j].visible = false;
+        }
+    } else {
+        for (var i = vehicles.length; i < points.length; i++) {
+            vehicles.pop();
+        }
+
+        for (var i = vehicles.length; i < points.length; i++) {
+            var pt = points[i];
+            var vehicle = new Vehicle(pt.x, pt.y);
+            vehicles.push(vehicle);
+        }
+    }
+    console.log("Cambio di tempo: " + time);
 }
