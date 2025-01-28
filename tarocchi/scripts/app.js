133092a912962a76440d349454628b35a5eb9183
F95875C <matteo.filippini@ivecogroup.com>
2025-01-28 13:14:30 +0100

    Modifica link web assoluto per descrizioni

--- tarocchi/scripts/app.js
+++ tarocchi/scripts/app.js
@@ -3,11 +3,11 @@ import { descrizioneStese } from "./descrizione_stese.js";
 
 // Mappa che associa il nome del file a un import dinamico
 const metodiMapping = {
-    "descrizione_metodi_giornaliere.js": "./descrizione_metodi_giornaliere.js",
-    "descrizione_metodi_settimanale.js": "./descrizione_metodi_settimanale.js",
-    "descrizione_metodi_mensile.js": "./descrizione_metodi_mensile.js",
-    "descrizione_metodi_annuale.js": "./descrizione_metodi_annuale.js",
-    "descrizione_metodi_futuro.js": "./descrizione_metodi_futuro.js",
+    "descrizione_metodi_giornaliere.js": "https://mattemn97.github.io/tarocchi/descrizioni/descrizione_metodi_giornaliere.js",
+    "descrizione_metodi_settimanale.js": "https://mattemn97.github.io/tarocchi/descrizioni/descrizione_metodi_settimanale.js",
+    "descrizione_metodi_mensile.js": "https://mattemn97.github.io/tarocchi/descrizioni/descrizione_metodi_mensile.js",
+    "descrizione_metodi_annuale.js": "https://mattemn97.github.io/tarocchi/descrizioni/descrizione_metodi_annuale.js",
+    "descrizione_metodi_futuro.js": "https://mattemn97.github.io/tarocchi/descrizioni/descrizione_metodi_futuro.js",
 };
 
 document.addEventListener("DOMContentLoaded", () => {
